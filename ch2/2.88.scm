0effea74f6d90db0ba3c1f29a1cf9217f14524fb
Andy Kitson <andrew.kitson@gmail.com>
2014-12-19 16:31:56 -0600

    move subtraction to term level

--- ch2/2.88.scm
+++ ch2/2.88.scm
@@ -24,3 +24,14 @@
 1 ]=> (sub b a)
 ;Value 187: (polynomial x (4 2) (2 4) (0 -5))
 
+; UPDATE! In 2.91, I had to subtract terms and not just polynomials, so switched everything over to do the subtraction at the level
+; of terms
+
+  (define (negate-list terms)
+    (mul-term-by-all-terms (make-term 0 -1) terms))
+
+  (define (sub-terms L1 L2)
+    (add-terms L1 (negate-list L2)))
+
+  (define (sub-poly a b)
+    (make-poly (variable a) (sub-terms (term-list a) (term-list b)))) 
