4d97ed18ead05c71f2299433182b989549244174
Andy Kitson <andrew.kitson@gmail.com>
2015-05-22 08:14:31 -0700

    clean up formatting, 4.2-3

--- ch4/4.3.scm
+++ ch4/4.3.scm
@@ -1,52 +1,67 @@
-; Rewrite eval so that the dispatch is done in data-directed style. Compare this with the data-directed differentiation procedure of 
-; exercise 2.73. (You may use the car of a compound expression as the type of the expression, as is appropriate for the syntax 
-; implemented in this section.)
+; Rewrite eval so that the dispatch is done in data-directed style. Compare
+; this with the data-directed differentiation procedure of exercise 2.73.
+; (You may use the car of a compound expression as the type of the expression,
+; as is appropriate for the syntax implemented in this section.)
 ; ========
 ; Alrighty. Let's recall the building blocks of a data-directed system:
 ; - data is tagged
-; - the tag on a data determines the procedure we use to operate on it 
-; - procedures are registered centrally and looked up by their tag 
-
-; We're already a good part of the way to a data-directed system, since the expressions are all tagged with their type. Except, of course,
-; the non-compound expressions. For those, we'll need special checks. Also, if we're not tagging procedure application like Louis 
-; wanted in 3.1 (and I don't think we should), we can't just default to the procedure in the table lookup. Intead, we'll have
-; to check the table for a procedure and apply it if we find one, but if not, do the application? check.  Also, we're stripping
+; - the tag on a data determines the procedure we use to operate on it
+; - procedures are registered centrally and looked up by their tag
+;
+; We're already a good part of the way to a data-directed system, since the
+; expressions are all tagged with their type. Except, of course, the
+; non-compound expressions. For those, we'll need special checks. Also, if
+; we're not tagging procedure application like Louis wanted in 3.1 (and I
+; don't think we should), we can't just default to the procedure in the table
+; lookup. Intead, we'll have to check the table for a procedure and apply it
+; if we find one, but if not, do the application? check. Also, we're stripping
 ; the tag before passing it along to the handling code.
 ;
 ; Here's our data-directed eval:
 (define (eval exp env)
-    (cond 
+    (cond
         ((self-evaluating? exp) exp)
         ((variable? exp) exp)
-        (else (let ((eval-proc (get (car exp) 'eval)))
-            (cond (eval-proc 
-                    (eval-proc exp (cdr exp)))
+        (else (let ((eval-proc (get (operator exp) 'eval)))
+            (cond (eval-proc
+                    ; (display (eq? eval-proc eval-if))
+                    (eval-proc (operands exp) env))
                   ((application? exp)
                     (apply (eval (operator exp) env)
                            (list-of-values (operands exp) env)))
                   (else (error "Unknown expression type -- EVAL" (list exp env))))))))
 
-; Our supporting code is below. It comes from 4.1 and 4.2 (for the eval pieces) and 3.25 for the table to use to store the procedures.
-; We haven't built out the table yet (will do that in a second), but this is enough to allow us to do the simple procedure 
-; applications we did in 4.2:
+; Our supporting code is below. It comes from 4.1 and 4.2 (for the eval pieces)
+; and 3.25 for the table to use to store the procedures. We haven't built out
+; the table yet (will do that in a second), but this is enough to allow us to
+; do the simple procedure applications we did in 4.2:
 
 1 ]=> (eval (list + 1 2 3) 'env)
 ;Value: 6
 1 ]=> (eval (list list 1 2 3) 'env)
 ;Value 2: (1 2 3)
 
-; I'm using list here because the exp is supposed to be a list. If I try to pass the expression itself as a parameter mit-scheme (the
-; interpreter our interpreter is running in) evaluates it before we can do anything with it.
+; I'm using list here because the expression is supposed to be a list. If I try
+; to pass the expression itself as a parameter mit-scheme (the interpreter our
+; interpreter is running in) evaluates it before we can do anything with it.
 ;
-; Now we can update the table to handle evaluating some other stuff. We'll do quotes since it doesn't depend on the environment 
-; for evaluation (we haven't yet touched on how the environment figures into the evaluation).
-(put 'quote 'eval (lambda (exp) exp))
+; Now we can update the table to handle evaluating some other stuff. We'll do
+; if since it doesn't depend on the environment for evaluation (SICP hasn't yet
+; elucidated how the environment figures into the evaluation).
+(define (eval-if exp env)
+    (define (if-predicate exp) (car exp))
+    (define (if-consequent exp) (cadr exp))
+    (define (if-alternative exp)
+        (if (not (null? (cddr exp)))
+            (caddr exp)
+            'false))
+    (if (true? (eval (if-predicate exp) env))
+        (eval (if-consequent exp) env)
+        (eval (if-alternative exp) env)))
+(put 'if 'eval eval-if)
 
-; And here we go:
-1 ]=> (eval (list list 'a 'b) 'env)
-;Value 3: (a b)
-
-
+; And here we go! Even works with nested values:
+1 ]=> (eval (list 'if 'false (list + 1 2) (list 'if 'true (list + 3 4) (list + 5 6))) 'env)
 
 ; And our supporting code:
 ;; Predicates
@@ -77,8 +92,9 @@
 
 
 
-; And here, finally, are our general data-directed support functions. We'll use our arbitrary key table from ex 3.25 and define get 
-; and put procedures that work on an instance of it:
+; And here, finally, are our general data-directed support functions. We'll
+; use our arbitrary key table from ex 3.25 and define get and put procedures
+; that work on an instance of it:
 
 (define (make-table same-key?)
     (define (assoc key records)
@@ -117,9 +133,9 @@
         'ok)
     (let ((local-table (list '*table* '*table-struct*)))
         (define (dispatch m)
-            (cond ((eq? m 'lookup-proc) 
+            (cond ((eq? m 'lookup-proc)
                     (lambda (keys) (lookup keys local-table)))
-                  ((eq? m 'insert-proc!) 
+                  ((eq? m 'insert-proc!)
                     (lambda (keys val) (insert! keys val local-table)))
                   ((eq? m 'show) (display local-table))))
         dispatch))
